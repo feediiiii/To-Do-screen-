8b7675ff618174d6d7af8c3099b2707e63a40834
Fedi <fediguizeniedu@gmal.com>
2022-10-10 22:44:05 +0000

    I added a prompt to take the name of our user to use it after in the web page + I added a message that salute our user and tell him about us with a 'I continue' word that if you click it it will be hidding that previous message and show you the web page of our ToDo

--- firstPageJs.js
+++ firstPageJs.js
@@ -1,3 +1,15 @@
+var userName = prompt("Can I have your name plz  🥺 ? ","tipe your name here ");
+userName= userName[0].toUpperCase()+userName.substring(1);
+
+$('#hiUser #hiUserMessage').text('Hi  '+userName+" , Welcome to your new world of studing ,We will be your new pair of studying")
+
+function disapear(){
+    $("#hiUser").hide("slow");
+}
+
+
+
+
 function wariLwa9t(){
     var lwa9t=new Date();
     sweya3=lwa9t.getHours()
@@ -14,20 +26,25 @@ $("#dateOutput").text(lwa9t.getDate()+"/"+lwa9t.getMonth()+"/"+lwa9t.getFullYear
         $("#dayOrNight").text("Good Morning ")
     }
     if ( sweya3 > 12 && sweya3 < 17){
-        $("#dayOrNight").text("saha ftourkom, have a quick nap and go back to work ");
+        $("#dayOrNight").text("saha ftourk "+userName+", have a quick nap and go back to work ");
     }
     if ( sweya3 >=17 && sweya3 < 18){
-        $("#dayOrNight").text("It's " + (sweya3-12).toString() +" PM, Take a rest u deserve it Champ ")
+        $("#dayOrNight").text("It's " + (sweya3-12).toString() +" PM, Take a rest u deserve it  "+userName)
     }
     if ( sweya3 >=18 && sweya3 < 21 ){
         $("#dayOrNight").text("It's " + (sweya3-12).toString() +" PM, Try to complete what you suppose to do today");
     }
     if ( sweya3 <= 23 && sweya3 >= 21 ){
-        $("#dayOrNight").text("It's " + (sweya3-12).toString() +" PM, If not urgent,try to complete the work tomorrow,enough work for today CHAMP !")
+        $("#dayOrNight").text("It's " + (sweya3-12).toString() +" PM, If not urgent,try to complete the work tomorrow,enough work for today "+userName+" !")
     }
     
 }
 
 setInterval(wariLwa9t,10)   // setInterval will refresh this function every 10 mili seconde :)
 
+function printOnPressing (){
+   var theCurrentVal =  $("#toDoInput").val();
+   $("ul").append("<li>"+ theCurrentVal +"</li>")
+
+}
 
